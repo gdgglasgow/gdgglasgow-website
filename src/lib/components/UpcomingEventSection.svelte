ea610a35aa34e151d23dba1c45da8b8a07ee60ad
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-09-10 20:14:59 +0100

    update about and upcoming event sections

--- src/lib/components/UpcomingEventSection.svelte
+++ src/lib/components/UpcomingEventSection.svelte
@@ -1,5 +1,36 @@
-<script>
+<script lang="ts">
 	import PrimaryButton from './PrimaryButton.svelte';
+
+	const event = {
+		title: 'DevFest Scotland 2023',
+		extra: '🏴󠁧󠁢󠁳󠁣󠁴󠁿',
+		description:
+			"DevFests are local tech conferences hosted by Google Developer Groups (GDGs) around the world, filled with talks, demos, and workshops about the latest in tech. This year, the GDG squads from Glasgow and Edinburgh are teaming up for DevFest Scotland 🏴󠁧󠁢󠁳󠁣󠁴󠁿 right in the heart of Glasgow. Dive into Google's newest stuff and more, learn from the experts, and network with the community!",
+		date: '2023-11-25',
+		// dateString: 'Sat, Nov 25, 9:00 AM (GMT)',
+		location: '105 Brunswick St - 105 Brunswick Street Glasgow City, G1 1TF',
+		link: 'https://gdg.community.dev/events/details/google-gdg-glasgow-presents-devfest-scotland-2023/cohost-gdg-glasgow'
+	};
+
+	const convertDate = (date: string) => {
+		const months = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December'
+		];
+		const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+		const d = new Date(date);
+		return `${days[d.getDay()]}, ${d.getDate()}, ${months[d.getMonth()]}, ${d.getFullYear()}`;
+	};
 </script>
 
 <section id="about" class="py-10">
@@ -9,19 +40,29 @@
 		>
 			Coming up...
 		</h2>
-		<h3 class="text-5xl">
-			<span class="">DevFest Scotland 2023</span>
-			🏴󠁧󠁢󠁳󠁣󠁴󠁿
-		</h3>
-		<div class="text-2xl mt-8">
-			DevFests are local tech conferences hosted by Google Developer Groups (GDGs) around the world,
-			filled with talks, demos, and workshops about the latest in tech. This year, the GDG squads
-			from Glasgow and Edinburgh are teaming up for DevFest Scotland 🏴󠁧󠁢󠁳󠁣󠁴󠁿 right in the heart of
-			Glasgow. Dive into Google's newest stuff and more, learn from the experts, and network with
-			the community!
-		</div>
-		<a class="block mt-10" href="https://gdg.community.dev/gdg-glasgow/">
-			<PrimaryButton>Book Tickets</PrimaryButton>
-		</a>
+		<article>
+			<h3 class="text-5xl">
+				<span class="">{event.title}</span>
+				{event.extra}
+			</h3>
+			<p class="text-2xl mt-8">
+				{event.description}
+			</p>
+			<div class="space-y-3 mt-8">
+				<div class="text-xl">
+					<span>🗓</span>{' '}
+					<time datetime={event.date}>
+						{convertDate(event.date)}
+					</time>
+				</div>
+				<div class="text-xl">
+					<span>📍</span>{' '}
+					{event.location}
+				</div>
+			</div>
+			<a class="block mt-10" href={event.link}>
+				<PrimaryButton>Book Tickets</PrimaryButton>
+			</a>
+		</article>
 	</div>
 </section>
