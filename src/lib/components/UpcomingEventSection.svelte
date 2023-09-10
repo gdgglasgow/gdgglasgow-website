dd96a63d50e1145417d83f681e4324be316c89b8
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-09-10 13:26:59 +0100

    upcoming event section, background update

---
+++ src/lib/components/UpcomingEventSection.svelte
@@ -0,0 +1,27 @@
+<script>
+	import PrimaryButton from './PrimaryButton.svelte';
+</script>
+
+<section id="about" class="py-10">
+	<div class="m-auto px-2 md:px-4 py-10 max-w-[1028px]">
+		<h2
+			class="text-2xl text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-900 mb-6"
+		>
+			Coming up...
+		</h2>
+		<h3 class="text-5xl">
+			<span class="">DevFest Scotland 2023</span>
+			🏴󠁧󠁢󠁳󠁣󠁴󠁿
+		</h3>
+		<div class="text-2xl mt-8">
+			DevFests are local tech conferences hosted by Google Developer Groups (GDGs) around the world,
+			filled with talks, demos, and workshops about the latest in tech. This year, the GDG squads
+			from Glasgow and Edinburgh are teaming up for DevFest Scotland 🏴󠁧󠁢󠁳󠁣󠁴󠁿 right in the heart of
+			Glasgow. Dive into Google's newest stuff and more, learn from the experts, and network with
+			the community!
+		</div>
+		<a class="block mt-10" href="https://gdg.community.dev/gdg-glasgow/">
+			<PrimaryButton>Book Tickets</PrimaryButton>
+		</a>
+	</div>
+</section>
