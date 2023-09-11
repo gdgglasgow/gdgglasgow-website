2afdc341019c07a13a5bd21a79c1e2068d851a7c
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-09-11 22:16:18 +0100

    add footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import LogoIcon from './LogoIcon.svelte';
+	const navLinks = [
+		{ label: 'About', href: '/#about', alt: 'About Us Section' },
+		{
+			label: 'Sponsors',
+			href: 'https://gdg.community.dev/gdg-glasgow/#local-partners',
+			alt: 'Sponsors Page'
+		},
+		{
+			label: 'Learn more',
+			href: 'https://gdg.community.dev/gdg-glasgow/',
+			alt: 'GDG Community official page'
+		}
+	];
+</script>
+
+<footer>
+	<div
+		class="container m-auto px-2 md:px-4 py-10 max-w-[1028px] border-t flex flex-col md:flex-row gap-4 md:gap-10"
+	>
+		<div class="flex items-center gap-2 m-auto md:mx-0">
+			<LogoIcon />
+			<div class="text-gray-500 dark:text-gray-400">
+				&copy; {new Date().getFullYear() || 2023}
+				<span>GDG Glasgow</span>
+			</div>
+		</div>
+		<div>
+			<ul class="flex gap-8 w-fit m-auto md:mx-0 justify-between md:justify-start">
+				{#each navLinks as { href, label }}
+					<li class="hover:underline">
+						<a {href} class="hover:underline">{label}</a>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</div>
+</footer>
