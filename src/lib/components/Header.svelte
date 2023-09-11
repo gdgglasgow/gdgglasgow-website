675f00539d037d276bacf2a032cc1c1c5d0b5359
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-09-12 00:09:45 +0100

    fix header responsiveness

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -18,7 +18,7 @@
 <header class="px-2 inset-0 w-full sticky top-[2rem] z-50">
 	<nav class="container mt-0 mx-auto">
 		<div
-			class="flex justify-between container m-auto w-full sm:w-fit gap-10 items-center px-8 py-3 bg-dark/90 dark:bg-stone-50/80 text-white dark:text-dark rounded-xl text-sm min-h-[3rem]"
+			class="flex flex-col md:flex-row md:justify-between container m-auto w-full sm:w-fit gap-3 md:gap-10 items-center px-8 py-3 bg-dark/90 dark:bg-stone-50/80 text-white dark:text-dark rounded-xl text-sm min-h-[3rem]"
 		>
 			<a href="/" aria-label="Go to the GDG Glasgow homepage">
 				<span
@@ -30,22 +30,24 @@
 					<span>GDG Glasgow</span>
 				</span>
 			</a>
-			<ul class="flex gap-10">
-				{#each navLinks as { href, label, alt }}
-					<li>
-						<a {href} class="hover:underline" aria-label={`Go to ${alt}`} title={`Go to ${alt}`}
-							>{label}</a
-						>
-					</li>
-				{/each}
-			</ul>
-			<div class="flex gap-3 items-center">
-				<button
-					aria-label="Toggle Theme"
-					class="btn--toggle w-4 h-4 rounded-full aspect-square"
-					tabindex="0"
-					on:click={onToggleClick}
-				/>
+			<div class="flex gap-8 w-fit m-auto md:mx-0">
+				<ul class="flex gap-8 justify-between md:gap-10">
+					{#each navLinks as { href, label, alt }}
+						<li>
+							<a {href} class="hover:underline" aria-label={`Go to ${alt}`} title={`Go to ${alt}`}
+								>{label}</a
+							>
+						</li>
+					{/each}
+				</ul>
+				<div class="flex gap-3 items-center">
+					<button
+						aria-label="Toggle Theme"
+						class="btn--toggle w-4 h-4 rounded-full aspect-square"
+						tabindex="0"
+						on:click={onToggleClick}
+					/>
+				</div>
 			</div>
 		</div>
 	</nav>
