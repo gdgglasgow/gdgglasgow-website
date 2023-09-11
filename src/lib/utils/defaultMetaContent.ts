e823ebebc0c4ebc08b7dd848cf3bb666cfcfb4bd
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-09-11 22:24:02 +0100

    update meta

--- src/lib/utils/defaultMetaContent.ts
+++ src/lib/utils/defaultMetaContent.ts
@@ -5,6 +5,6 @@
 export default {
     title: 'Google Developers Group Glasgow',
     description: "GDG Glasgow is a volunteer-led tech community focused on sharing the latest from Google and more. We offer events on both technological advancements and essential soft skills. Serving the Scottish tech scene, we welcome developers, tech enthusiasts, and the curious to join, learn, and network.",
-    // image: "https://afhro.org/wp-content/uploads/2023/06/Publishing-logo.png",
+    image: "/images/gdg-glasgow-preview-banner.webp",
     url: "https://gdgglasgow.com",
 };
